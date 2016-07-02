90f6c37f5c35c1f41bdd66f37fb41f20605a5df5
211217613 <rubenavazquez@gmail.com>
2016-07-01 20:22:26 -0500

    Add -v flag for verbose output

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,20 +7,20 @@ echo $BASEDIR
 
 #vim
 echo "linking .vimrc"
-ln -s -f ${BASEDIR}/.vimrc ~/.vimrc
+ln -sv -f ${BASEDIR}/.vimrc ~/.vimrc
 
 #git
 echo "linking glob gitconfig and gitignore"
-ln -s -f ${BASEDIR}/git/.gitignore ~/.gitignore
-ln -s -f ${BASEDIR}/git/.gitconfig ~/.gitconfig
+ln -sv -f ${BASEDIR}/git/.gitignore ~/.gitignore
+ln -sv -f ${BASEDIR}/git/.gitconfig ~/.gitconfig
 
 #bashrc
 echo "linking bashrc"
-ln -s -f ${BASEDIR}/bash/.bashrc ~/.bashrc
+ln -sv -f ${BASEDIR}/bash/.bashrc ~/.bashrc
 
 #subl
-echo "linking subl"
-ln -s -f ${BASEDIR}/Python3.sublime-build /home/$USER/.config/sublime-text3/Packages/User/Python3.sublime-build
+#echo "linking subl"
+#ln -sv -f ${BASEDIR}/Python3.sublime-build /home/$USER/.config/sublime-text3/Packages/User/Python3.sublime-build
 #ADD
 #/home/rvazquez/.config/sublime-text-3/Packages/User/C++11.sublime-build
 #i3-wm
