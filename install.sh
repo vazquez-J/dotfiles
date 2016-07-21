de2601e448b52221229da331e3621d55f4a60d96
211217613 <rubenavazquez@gmail.com>
2016-07-21 15:53:32 -0500

    Add subl notes and fix wrong entry

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $BASEDIR
 
+#ack 
+echo "*** linking ackrc***"
+echo ""
+ln -sv -f ${BASEDIR}/ack/.ackrc ~
+
 #vim
 echo "linking .vimrc"
 ln -sv -f ${BASEDIR}/.vimrc ~/.vimrc
@@ -20,7 +25,13 @@ ln -sv -f ${BASEDIR}/bash/.bashrc ~/.bashrc
 
 #subl
 echo "***linking subl***"
-ln -sv -f ${BASEDIR}/subl/ython3.sublime-build /home/$USER/.config/sublime-text3/Packages/User/Python3.sublime-build
+ln -sv -f ${BASEDIR}/subl/Python3.sublime-build /home/$USER/.config/sublime-text3/Packages/User/Python3.sublime-build
+
+#Install subl
+#Check for deb package
+#If deb package downloaded dpkg -i it
+#Else call python script
+
 #ADD
 #/home/rvazquez/.config/sublime-text-3/Packages/User/C++11.sublime-build
 #i3-wm
