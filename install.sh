100f41e8e68fdba8e9a06f9ef310b14e2d1057d5
211217613 <rubenavazquez@gmail.com>
2016-12-30 19:05:06 -0800

    Add vbox function

--- install.sh
+++ install.sh
@@ -35,6 +35,13 @@ function vundle(){
 	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 }
 
+function vbox_install(){
+	sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' > /etc/apt/sources.list.d/virtualbox.list" && \
+	wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && \
+	sudo apt-get update && \
+	sudo apt-get install virtualbox-5.1
+}
+
 function apt_packages(){
     echo 'Installing packages...'
     sleep 2
