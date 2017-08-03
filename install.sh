62bc6758f7886c61373626312f23cb2d4c011967
211217613 <rubenavazquez@gmail.com>
2017-08-03 11:02:57 -0700

    Add installing of spotify signing keys and add spotify client install to local function

--- install.sh
+++ install.sh
@@ -45,8 +45,8 @@ function apt_packages(){
     echo 'Installing packages...'
     echo 'Adding PPAs'
     sleep 2
-    install_ppas()
-    sudo apt install -qq -y  zip xbacklight xflux git vim spotify-client meld build-essential ack-grep geoclue-2.0 libssl-dev
+#    install_ppas()
+    sudo apt install -qq -y  zip xbacklight xflux git vim meld build-essential ack-grep geoclue-2.0 libssl-dev
 	
 }
 
@@ -63,7 +63,7 @@ function remove_packages(){
     sudo apt purge -y --force-yes pidgin thunderbird brasero k3b
 }
 
-function download_sublime() {
+function download_sublime(){
     if test -e /home/user/Dev/dotfiles/bin/sublime_text_*.deb; then
         echo "Sublime text already installed" >&2
     else
@@ -151,6 +151,12 @@ function geoclue() {
 }
 
 function spotify() {
+    echo 'Installing Spotify keys and ppa'
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+    echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt update
+    sudo apt install spotify-client
+    echo 'Linking modified desktop file to support HiDPI'
     sudo ln -svf spotify.desktop /usr/share/applications/spotify.desktop
     # ln -s -f ${BASEDIR}
 }
