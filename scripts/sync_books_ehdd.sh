711d3e28ffc96a9aa7ebf60071718c60d255f861
211217613 <rubenavazquez@gmail.com>
2019-01-13 11:48:27 -0500

    add else clause echos skipping operation.

--- scripts/sync_books_ehdd.sh
+++ scripts/sync_books_ehdd.sh
@@ -3,4 +3,6 @@
 if [ -d /media/$USER/My\ Passport/Books/ ]; then
 	echo 'running rsync'
 	rsync --delete -avsh  "/home/$USER/Books/" /media/rvazquez/My\ Passport/Books/
+else
+	echo 'Drive not mounted.... skipping operation!'
 fi
